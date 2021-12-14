6fd3a66503e8f905f05cd74ee6c6e0c07bad9e95
TiaZheng <71717366+TiaZheng@users.noreply.github.com>
2021-12-14 12:26:58 -0500

    hi

---
+++ test/pages/rules.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div class="container">
+        <h1 class="h1-container">How to Play</h1>
+        <section class="rules-container">test</section>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #0E1555;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+}
+
+.h1-container{
+    background-color: #FD367E;
+    color: white
+}
+
+.rules-container{
+    background-color: thistle;
+}
+</style>
