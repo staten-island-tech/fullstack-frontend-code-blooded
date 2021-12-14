cbb4ae2c9fa8fc54d359686bb9efbbcb2540bb4a
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-12-14 12:27:39 -0500

    commit

---
+++ test/pages/friends.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main">
+        <button>?</button>
+        <h1>Friends</h1>
+    </div>
+</template>
+
+<style scoped>
+    .main{
+        height: 100vh;
+        background-color: purple;
+    }
+</style>
