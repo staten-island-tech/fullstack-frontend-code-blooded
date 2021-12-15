3f149e34492d5757ac389d14a66385bff55e1202
TiaZheng <71717366+TiaZheng@users.noreply.github.com>
2021-12-15 12:27:41 -0500

    wordsss

--- test/pages/rules.vue
+++ test/pages/rules.vue
@@ -2,7 +2,12 @@
   <div>
       <div class="container">
         <h1 class="h1-container">How to Play</h1>
-        <section class="rules-container">test</section>
+        <section class="rules-container"> Welcome один brought by Coded-Blooded! 
+           This web app allows you to play UNO, the popular real-time card game designed by Merle Robbins in 1971
+           <a href="https://en.wikipedia.org/wiki/Uno_(card_game)#:~:text=History%20The%20game%20was%20originally%20developed%20in%201971,local%20businesses%20began%20to%20sell%20it%20as%20well.">(Wikipedia)</a>. 
+           The game is a race to be the first person to play the very last card in your hand. and the computer will handle all the details of dealing cards and keeping score.
+
+If you haven't played before, don't worry! We'll explain the rules below.</section>
       </div>
   </div>
 </template>
@@ -26,10 +31,16 @@ export default {
 
 .h1-container{
     background-color: #FD367E;
-    color: white
+    color: white;
+    margin: 5rem 0rem 7rem;
+    padding: 1.1rem 5rem .8rem;
+    border-radius: 1rem;
 }
 
 .rules-container{
-    background-color: thistle;
+    background-image: linear-gradient(#932B77,#0E1555);
+    height: 100vh;
+    border-radius: 1rem;
+    width: 55vw;
 }
 </style>
