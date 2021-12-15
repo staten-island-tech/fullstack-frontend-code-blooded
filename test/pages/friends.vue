128062768bc40b3a3f80fa0242bda2d4dd66a47c
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-12-15 12:27:34 -0500

    commit

--- test/pages/friends.vue
+++ test/pages/friends.vue
@@ -9,28 +9,50 @@
                 <li class="norm">HTML</li>
                 <li class="norm">Vue</li>
             </ul>
+            <button class="edit">Edit Friends</button>
         </div>
+        
     </div>
 </template>
 
 <style scoped>
     .main{
         height: 100vh;
-        background-color: rgb(63, 0, 63);
+        background-color: #0E1555;
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 11rem;
     }
     ul{
         list-style: none;
+        color: #fff;
+        height: 50vh;
+        width: 50vw;
         display: flex;
         flex-direction: column;
         align-items: center;
-        color: #fff;
+        font-weight: 500;
+        background: linear-gradient(#fff, #000);
+        border-radius: 2rem;
     }
     li{
         margin: 1rem;
-        font-size: 1rem;
+        font-size: 3rem;
     }
     .special{
-        font-size: 2rem;
+        font-size: 4rem;
         margin-bottom: 2rem;
+        color: #FD367E;
+    }
+    .edit{
+        font-size: 2.5rem;
+        background-color: #FD367E;
+        font-weight: bolder;
+        border-radius: 1.5rem;
+        margin-top: 2rem;
     }
 </style>
