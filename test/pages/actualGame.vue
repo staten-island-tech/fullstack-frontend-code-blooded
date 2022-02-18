d3e5a2b0dd24413f889c4cf91ec1986c3e381473
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-02-18 12:09:00 -0500

    logo

---
+++ test/pages/actualGame.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <h1>NOTHING HERE YET</h1>
+    </div>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Tomorrow:wght@400;600;900&display=swap');
+/* VARIABLES */
+    :root{
+     --background-color:#0E1555;
+     --secondary-color:#4E1184;
+     --third-color:#932B77;
+     --fourth-color:#FD367E;
+     --font-color:#ffff;
+     --heavy-weight:900;
+     --medium-weight:600;
+     --thin-weight:400;
+    }
+/* Common CSS */
+  #app{
+    background-color:#0E1555;
+    max-width: 100vw;
+    height: 100vh;
+    font-family: 'Tomorrow', sans-serif;
+    margin:0 auto;
+    overflow: hidden;
+    font-size: 100%;
+    vertical-align: baseline;
+    box-sizing: border-box;
+  }
+</style>
