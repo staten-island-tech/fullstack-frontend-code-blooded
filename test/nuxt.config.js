b2f3f8d0111663b2943a30147a1a306c0d3d7389
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-03-31 11:49:04 -0400

    efndjbngvjgbf

--- test/nuxt.config.js
+++ test/nuxt.config.js
@@ -20,7 +20,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/balm-ui'
+    '@/plugins/balm-ui',
+    "~/plugins/auth0.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ test/plugins/auth0.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "../auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
