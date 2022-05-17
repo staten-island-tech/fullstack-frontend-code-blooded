5c0b6093ff874c437dafabeb308c22dfa5583144
lisali-liang <lisaliliang27@gmail.com>
2022-05-17 12:06:24 -0400

    socket is in index

--- test/components/pages-comp/ActualGame.vue
+++ test/components/pages-comp/ActualGame.vue
@@ -1,135 +1,138 @@
 <template>
-    <div id="container">
-      <section class="gamepage">
-        <div class="player-container">
-          <div v-for="player in playerData" :key=player.id class="players">
-            <div class="player-box" :style="{borderColor: player.playerColor}">
-
-              <h1 class="username" :style="{backgroundColor: player.playerColor}">{{player.playerName}}</h1>
-              <h2 class="cardLeft">{{player.cardData}} cards in hand</h2>
-            </div>
+  <div id="container">
+    <section class="gamepage">
+      <div class="player-container">
+        <div v-for="player in playerData" :key="player.id" class="players">
+          <div class="player-box" :style="{ borderColor: player.playerColor }">
+            <h1
+              class="username"
+              :style="{ backgroundColor: player.playerColor }"
+            >
+              {{ player.playerName }}
+            </h1>
+            <h2 class="cardLeft">{{ player.cardData }} cards in hand</h2>
           </div>
         </div>
-        <div class="cardStack">
-          <div class="cardHand">
-            <h2 class="deck-numbers">deck: 93 | used: 18</h2>
-            <div class="middleBox">
-              <div class="icon"><h3 class="slash">//</h3></div>
-              <div class="time-player">
-                <div class="timeRow">
-                  <h1 class="time sizeLarge" :style="{color: playerColor}">{{playerName}} </h1>
-                  <h1 class="time sizeLarge"> has {{playerTime}} seconds left</h1>
-                </div>
-                <div class="timeRow">
-                  <h2 class="time sizeSmall" :style="{color: nextColor}">{{playerNext}}</h2>
-                  <h2 class="time sizeSmall"> is up next</h2>
-                </div>
+      </div>
+      <div class="cardStack">
+        <div class="cardHand">
+          <h2 class="deck-numbers">deck: 93 | used: 18</h2>
+          <div class="middleBox">
+            <div class="icon"><h3 class="slash">//</h3></div>
+            <div class="time-player">
+              <div class="timeRow">
+                <h1 class="time sizeLarge" :style="{ color: playerColor }">
+                  {{ playerName }}
+                </h1>
+                <h1 class="time sizeLarge">
+                  has {{ playerTime }} seconds left
+                </h1>
+              </div>
+              <div class="timeRow">
+                <h2 class="time sizeSmall" :style="{ color: nextColor }">
+                  {{ playerNext }}
+                </h2>
+                <h2 class="time sizeSmall">is up next</h2>
               </div>
             </div>
-            <div class="cardsBox">
-                {{allCards.cardImg}}
-            </div>
           </div>
-          <div class="gameActions">
-            <button class="drawACard">draw a card</button>
-            <button class="pass">pass</button>
-          </div>
-          <div class="table">
-            <div class="cardOnTable">
-              <h2 class="tableLabel">table</h2>
-            </div>
-            <End/>
+          <div class="cardsBox">
+            <!-- {{allCards.cardImg}} -->
           </div>
         </div>
-        
-      </section>
-      
-    </div>
-
+        <div class="gameActions">
+          <button class="drawACard">draw a card</button>
+          <button class="pass">pass</button>
+        </div>
+        <div class="table">
+          <div class="cardOnTable">
+            <h2 class="tableLabel">table</h2>
+          </div>
+          <End />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 // import allCards from '../../pages/allCards.js';
-import End from '@/components/reg-comp/End.vue';
+import End from '@/components/reg-comp/End.vue'
 export default {
   name: 'ActualGame',
- components: {
-      End
-    },
-data(){
-  return{
-    // allCards,
-    playerName:"Vue",
-    playerColor:"#71D097",
-    playerTime:22,
-    playerNext:"HTML",
-    nextColor:"#49C8FF",
-    playerData:[
-      {
-        playerName: "placeholder",
-        cardData:0,
-        id:1,
-        playerColor:"#71D097",
-      },
-      {
-        playerName: "placeholder",
-        cardData:0,
-        id:2,
-        playerColor: "#49C8FF"
-      },
-      {
-        playerName: "placeholder",
-        cardData:0,
-        id:3,
-        playerColor: "#EE914D"
-      },      
-    ],
-
-
-
-
-  }
-},
-  methods: {
-      goIndex() {
-        this.$router.push('/');
-      },
+  components: {
+    End,
+  },
+  data() {
+    return {
+      // allCards,
+      playerName: 'Vue',
+      playerColor: '#71D097',
+      playerTime: 22,
+      playerNext: 'HTML',
+      nextColor: '#49C8FF',
+      playerData: [
+        {
+          playerName: 'placeholder',
+          cardData: 0,
+          id: 1,
+          playerColor: '#71D097',
+        },
+        {
+          playerName: 'placeholder',
+          cardData: 0,
+          id: 2,
+          playerColor: '#49C8FF',
+        },
+        {
+          playerName: 'placeholder',
+          cardData: 0,
+          id: 3,
+          playerColor: '#EE914D',
+        },
+      ],
     }
+  },
+  methods: {
+    goIndex() {
+      this.$router.push('/')
+    },
+  },
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Tomorrow:wght@400;600;900&display=swap');
 /* VARIABLES */
-    *{
-     --background-color:#0E1555;
-     --secondary-color:#4E1184;
-     --third-color:#932B77;
-     --fourth-color:#FD367E;
-     --fifth-color:#880000;
-     --font-color:#ffff;
-     --heavy-weight:900;
-     --medium-weight:600;
-     --thin-weight:400;
-    }
+* {
+  --background-color: #0e1555;
+  --secondary-color: #4e1184;
+  --third-color: #932b77;
+  --fourth-color: #fd367e;
+  --fifth-color: #880000;
+  --font-color: #ffff;
+  --heavy-weight: 900;
+  --medium-weight: 600;
+  --thin-weight: 400;
+}
 /* Common CSS */
-  #container{
-    background-color:var(--background-color);
-    max-width: 100vw;
-    height: 100vh;
-    font-family: 'Tomorrow', sans-serif;
-    margin:0 auto;
-    overflow: hidden;
-    font-size: 100%;
-    vertical-align: baseline;
-    box-sizing: border-box;
-  }
-  .mdc-typography {
+#container {
+  background-color: var(--background-color);
+  max-width: 100vw;
+  height: 100vh;
+  font-family: 'Tomorrow', sans-serif;
+  margin: 0 auto;
+  overflow: hidden;
+  font-size: 100%;
+  vertical-align: baseline;
+  box-sizing: border-box;
+}
+.mdc-typography {
   margin: 0;
 }
 
 /* top part of page */
-.player-container{
+.player-container {
   height: 40vh;
   width: 100vw;
   display: flex;
@@ -139,150 +142,143 @@ data(){
   text-align: center;
 }
 
-  .player-box{
-    background-color: var(--background-color);
-    width: 25vw;
-    height: 27vh;
-    border: solid;
-    border-radius: 3rem;
-    border-width: 0.4rem;
-        color: var(--font-color);
+.player-box {
+  background-color: var(--background-color);
+  width: 25vw;
+  height: 27vh;
+  border: solid;
+  border-radius: 3rem;
+  border-width: 0.4rem;
+  color: var(--font-color);
+}
 
-  }
+.username {
+  width: 10vw;
+  padding: 1rem;
+  position: relative;
+  bottom: 2rem;
+  border-radius: 1.5rem;
+  font-weight: var(--medium-weight);
+  text-transform: uppercase;
+  overflow: hidden;
+}
 
-  .username{
-    width: 10vw;
-    padding: 1rem;
-    position: relative;
-    bottom: 2rem;
-    border-radius: 1.5rem;
-    font-weight: var(--medium-weight);
-    text-transform: uppercase;
-    overflow: hidden;
-  }
-
-  .cardLeft{
-    font-weight: var(--medium-weight); 
-    transform: translate(0rem, 2.5rem);
-    font-size: 2rem;
-  }
-  /* Bottom */
-  .cardStack{
-    display:flex;
-    justify-content: center;
-    text-align: center;
-    width:95vw;
-    height:60vh;
-    margin:0 auto; 
-  }
+.cardLeft {
+  font-weight: var(--medium-weight);
+  transform: translate(0rem, 2.5rem);
+  font-size: 2rem;
+}
+/* Bottom */
+.cardStack {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  width: 95vw;
+  height: 60vh;
+  margin: 0 auto;
+}
 /* Bottom Top*/
-.cardHand{
-    display:flex;
-    flex-direction:column;
-    text-transform: uppercase;
-    width:40vw;
-    color:var(--font-color);
-    margin:0 auto;
-  } 
-  .middleBox{
-    display: flex;
-    width:100%;
-    height:10rem;
-  }
-  .timeRow{
-    display: flex;
-        height: 40%;
-    margin-top: -2%;
-  }
-  .sizeLarge{
-    font-size: 2rem;
-  }
-  .sizeSmall{
-    font-size: 1.5rem;
-  }
-  .icon{
-    background-color: #207B00;
-    width: 3.5vw;
-    height:8vh;;
-    align-items: center;
-    justify-content: center;
-    font-size: 2.5rem;
-    border-radius: 50%;
-    padding:.5rem;
-    
-  }
-  .slash{
-    position:relative;
-    bottom:30%;
-    /* position: absolute; */
-  }
-  .cardsBox{
-    width:100%;
-    height:60%;
-  }
+.cardHand {
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  width: 40vw;
+  color: var(--font-color);
+  margin: 0 auto;
+}
+.middleBox {
+  display: flex;
+  width: 100%;
+  height: 10rem;
+}
+.timeRow {
+  display: flex;
+  height: 40%;
+  margin-top: -2%;
+}
+.sizeLarge {
+  font-size: 2rem;
+}
+.sizeSmall {
+  font-size: 1.5rem;
+}
+.icon {
+  background-color: #207b00;
+  width: 3.5vw;
+  height: 8vh;
+  align-items: center;
+  justify-content: center;
+  font-size: 2.5rem;
+  border-radius: 50%;
+  padding: 0.5rem;
+}
+.slash {
+  position: relative;
+  bottom: 30%;
+  /* position: absolute; */
+}
+.cardsBox {
+  width: 100%;
+  height: 60%;
+}
 
-  .deck-numbers{
-    width:40%;
-    left: 0;
-    margin: 0;
-  } 
+.deck-numbers {
+  width: 40%;
+  left: 0;
+  margin: 0;
+}
 
 /* Bottom Middle*/
-  .gameActions{
-    display:flex;
-    flex-direction:column;
-    
-    width:27vw;
-    /* background-color: red; */
-  }
-  .drawACard,
-  .pass{
-    width:75%;
-    margin:0 auto;
-    font-family: 'Tomorrow', sans-serif;
-    font-weight:var(--medium-weight);
-    border-radius: 20px;
-    margin-top:1rem;
-    color:var(--font-color);
-    font-size:2rem;
-    padding:.5rem;
-    text-transform: uppercase;
-  }
-  .drawACard{
-    margin-top:60%;
-    background-color: var(--fourth-color);
-    border-color: var(--fourth-color);
-  }
-  .pass{
-    background-color: rgba(147, 43, 119, 1);
-     border-color: rgba(147, 43, 119, 1);
-  }
+.gameActions {
+  display: flex;
+  flex-direction: column;
+
+  width: 27vw;
+  /* background-color: red; */
+}
+.drawACard,
+.pass {
+  width: 75%;
+  margin: 0 auto;
+  font-family: 'Tomorrow', sans-serif;
+  font-weight: var(--medium-weight);
+  border-radius: 20px;
+  margin-top: 1rem;
+  color: var(--font-color);
+  font-size: 2rem;
+  padding: 0.5rem;
+  text-transform: uppercase;
+}
+.drawACard {
+  margin-top: 60%;
+  background-color: var(--fourth-color);
+  border-color: var(--fourth-color);
+}
+.pass {
+  background-color: rgba(147, 43, 119, 1);
+  border-color: rgba(147, 43, 119, 1);
+}
 /* Bottom Right */
-.table{
-    display:flex;
-    flex-direction:column;
-    text-transform: uppercase;
-    width:33vw;
-    /* background-color: yellow; */
-  }
-  .cardOnTable{
-    width:90%;
-    height:70%;
-    border-radius: 2rem;
-    border:solid .4rem;
-    border-color:var(--fourth-color);
-    color:var(--fourth-color);
-    font-family: 'Tomorrow', sans-serif;
-  }
-  .tableLabel{
-    font-weight:600;
-    font-size:2rem;
-    margin-top:1rem;
-    letter-spacing:.1rem;
-  }
-
-
+.table {
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  width: 33vw;
+  /* background-color: yellow; */
+}
+.cardOnTable {
+  width: 90%;
+  height: 70%;
+  border-radius: 2rem;
+  border: solid 0.4rem;
+  border-color: var(--fourth-color);
+  color: var(--fourth-color);
+  font-family: 'Tomorrow', sans-serif;
+}
+.tableLabel {
+  font-weight: 600;
+  font-size: 2rem;
+  margin-top: 1rem;
+  letter-spacing: 0.1rem;
+}
 </style>
-
-
-
--- test/pages/index.vue
+++ test/pages/index.vue
@@ -2,173 +2,189 @@
   <div id="app">
     <div class="btn">
       <button class="help"><a href="rules">?</a></button>
-      <Sign/>
+      <Sign />
     </div>
-    <Join v-show="isJoinVisible"/>
+    <Join v-show="isJoinVisible" />
 
-    <ActualGame></ActualGame>
+    <ActualGame v-show="testingComp"></ActualGame>
 
-    <div v-show="isJoinNotVisible" class="options" >
-      <h1 class="logo">code-blooded</h1>
-      <input ref="userName" class="input" type="text" placeholder="enter a name" name="username" required>
+    <div class="landing-wrapper" v-show="wrapper">
+      <div v-show="isJoinNotVisible" class="options">
+        <h1 class="logo">code-blooded</h1>
+        <input
+          ref="userName"
+          class="input"
+          type="text"
+          placeholder="enter a name"
+          name="username"
+          required
+        />
         <button class="start" @click="goGame">start a game</button>
-      <button class="join" @click="showModal">show join code</button>
+        <button class="join" @click="showModal">show join code</button>
+      </div>
     </div>
+
     <span class="apcsp">apcsp project</span>
   </div>
 </template>
 
 <script>
-import Join from '@/components/reg-comp/Join.vue';
-import Sign from '@/components/reg-comp/Sign.vue';
+import io from 'socket.io-client'
+
+import Join from '@/components/reg-comp/Join.vue'
+import Sign from '@/components/reg-comp/Sign.vue'
 
 // pages comp
-import ActualGame from '@/components/reg-comp/ActualGame.vue';
+import ActualGame from '@/components/pages-comp/ActualGame.vue'
 
-  //   <Modal v-show="isModalVisible" @close="closeModal"/>
-  export default {
-    name: 'App',
-    components: {
-      Sign,
-      Join,
+//   <Modal v-show="isModalVisible" @close="closeModal"/>
+export default {
+  name: 'App',
+  components: {
+    Sign,
+    Join,
 
-      // page
+    // page
 
-      ActualGame,
-    },
-    data() {
-      return {
-        isJoinVisible: false,
-        isJoinNotVisible:true,
-        username: null,
-      };
-    },
-    methods: {
-      showModal() {
-        this.isJoinVisible = true;
-        this.isJoinNotVisible = false;
-        this.username = this.$refs.userName.value;
-      },
-      goGame(){
-        this.$router.push('/game');
-      
-        this.username = this.$refs.userName.value;
-      },
-      closeModal() {
-        this.isJoinVisible = false;
-        this.isJoinNotVisible = true;
-      },
+    ActualGame,
+  },
+  data() {
+    return {
+      isJoinVisible: false,
+      isJoinNotVisible: true,
+      username: null,
+      testingComp: false,
+      wrapper: true,
     }
-  };
+  },
+  methods: {
+    showModal() {
+      this.isJoinVisible = true
+      this.isJoinNotVisible = false
+      // this.username = this.$refs.userName.value;
+    },
+    goGame() {
+      // this.$router.push('/game');
+      // this.username = this.$refs.userName.value;
+      this.testingComp = true
+      this.wrapper = false
+      this.socketInstance = io('http://localhost:3001')
+    },
+    closeModal() {
+      this.isJoinVisible = false
+      this.isJoinNotVisible = true
+    },
+  },
+}
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Tomorrow:wght@400;600;900&display=swap');
 /* VARIABLES */
-:root{
-  --background-color:#0E1555;
-  --secondary-color:#4E1184;
-  --third-color:#932B77;
-  --fourth-color:#FD367E;
-  --font-color:#ffff;
-  --heavy-weight:900;
-  --medium-weight:600;
-  --thin-weight:400;
+:root {
+  --background-color: #0e1555;
+  --secondary-color: #4e1184;
+  --third-color: #932b77;
+  --fourth-color: #fd367e;
+  --font-color: #ffff;
+  --heavy-weight: 900;
+  --medium-weight: 600;
+  --thin-weight: 400;
 }
 .mdc-typography {
   margin: 0;
 }
 /* Common CSS */
-  #app{
-    background-color: var(--background-color);
-    max-width: 100%;
-    height: 100vh;
-    font-family: 'Tomorrow', sans-serif;
-    margin:0 auto;
-  }
-  button{
-    font-family: 'Tomorrow', sans-serif;
-    font-weight:var(--medium-weight);
-  }
-  a{
-    text-decoration: none;
-  }
-  /* ? Button */
-  .help{
-    background-color: gray;
-    font-weight: var(--heavy-weight);
-    width:3rem;
-    height:3rem;
-    font-size: 2rem;
-    position:absolute;
-  }
-  /* FLEX BOX FOR BUTTONS */
-  .options,
-  .join-contain{
-    display:flex;
-    justify-content: center;
-    margin:0 auto;
-    align-items: center;
-  }
-  .options{
-    width:90vw;
-    height:90vh;
-    flex-direction: column;
-  }
-  .start,
-  .join,
-  input{
-    border-radius: 26px;
-    width:32%;
-    font-size:3rem;
-    margin-top:1rem;
-  }
-  input{
-    border-color: var(--fourth-color);
-    border-width: .3rem;
-    color:var(--font-color);
-    font-size:3.5rem;
-    background-color:var(--background-color);
-    text-align: center;
-  }
-  ::placeholder {
-  padding:0.5rem;
-  color:var(--font-color);
+#app {
+  background-color: var(--background-color);
+  max-width: 100%;
+  height: 100vh;
+  font-family: 'Tomorrow', sans-serif;
+  margin: 0 auto;
+}
+button {
+  font-family: 'Tomorrow', sans-serif;
+  font-weight: var(--medium-weight);
+}
+a {
+  text-decoration: none;
+}
+/* ? Button */
+.help {
+  background-color: gray;
+  font-weight: var(--heavy-weight);
+  width: 3rem;
+  height: 3rem;
+  font-size: 2rem;
+  position: absolute;
+}
+/* FLEX BOX FOR BUTTONS */
+.options,
+.join-contain {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  align-items: center;
+}
+.options {
+  width: 90vw;
+  height: 90vh;
+  flex-direction: column;
+}
+.start,
+.join,
+input {
+  border-radius: 26px;
+  width: 32%;
+  font-size: 3rem;
+  margin-top: 1rem;
+}
+input {
+  border-color: var(--fourth-color);
+  border-width: 0.3rem;
+  color: var(--font-color);
+  font-size: 3.5rem;
+  background-color: var(--background-color);
+  text-align: center;
+}
+::placeholder {
+  padding: 0.5rem;
+  color: var(--font-color);
   font-family: 'Tomorrow', sans-serif;
   font-weight: var(--thin-weight);
 }
-  .start{
-    background-color: var(--fourth-color);
-    color:var(--background-color);
-    font-size:3rem;
-    border-color: var(--fourth-color);
-    padding:.5rem;
-  }
-  .join{
-    background-color: var(--secondary-color);
-    color:var(--fourth-color);
-    font-size:3rem;
-    border-color: var(--secondary-color);
-    padding:.5rem;
-  }
-  /* SIGN IN BUTTON */
-  .sign-in{
-    background-color: var(--font-color);
-    color:var(--background-color);
-    border-radius: 25px;
-    height:3rem;
-    width:9rem;
-    position:absolute;
-    right:2rem;
-    top:2rem;
-    font-family: 'Tomorrow', sans-serif;
-    font-weight: var(--heavy-weight);
-     font-size:1.5rem;
-  }
-  /* CODE BLOODED HEADER */
-  .logo{
-    color:var(--font-color);
-    font-size: 6rem;
-  margin-bottom:1rem;
+.start {
+  background-color: var(--fourth-color);
+  color: var(--background-color);
+  font-size: 3rem;
+  border-color: var(--fourth-color);
+  padding: 0.5rem;
+}
+.join {
+  background-color: var(--secondary-color);
+  color: var(--fourth-color);
+  font-size: 3rem;
+  border-color: var(--secondary-color);
+  padding: 0.5rem;
+}
+/* SIGN IN BUTTON */
+.sign-in {
+  background-color: var(--font-color);
+  color: var(--background-color);
+  border-radius: 25px;
+  height: 3rem;
+  width: 9rem;
+  position: absolute;
+  right: 2rem;
+  top: 2rem;
+  font-family: 'Tomorrow', sans-serif;
+  font-weight: var(--heavy-weight);
+  font-size: 1.5rem;
+}
+/* CODE BLOODED HEADER */
+.logo {
+  color: var(--font-color);
+  font-size: 6rem;
+  margin-bottom: 1rem;
   background-image: linear-gradient(
     -225deg,
     #41327a 0%,
@@ -179,33 +195,31 @@ import ActualGame from '@/components/reg-comp/ActualGame.vue';
   background-size: auto auto;
   background-clip: border-box;
   background-size: 200% auto;
-   background-clip: text;
+  background-clip: text;
   text-fill-color: transparent;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   animation: textclip 2s linear infinite;
   display: inline-block;
-  }
-  @keyframes textclip {
+}
+@keyframes textclip {
   to {
     background-position: 200% center;
   }
 }
-  /* BOTTOM LOGO THINGY */
-  .apcsp{
-    background-color: var(--third-color);
-    bottom:0;
-    position:absolute;
-    padding:1rem;
-     border-radius: 5px 30px 5px 5px;
-  }
-  /* Join Components */
-  .join-contain{
-    margin:0 auto;
-    justify-content: center;
-    align-items: center;
-    width:49%;
-  }
-
-
+/* BOTTOM LOGO THINGY */
+.apcsp {
+  background-color: var(--third-color);
+  bottom: 0;
+  position: absolute;
+  padding: 1rem;
+  border-radius: 5px 30px 5px 5px;
+}
+/* Join Components */
+.join-contain {
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  width: 49%;
+}
 </style>
