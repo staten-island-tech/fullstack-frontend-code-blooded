9d71b20e877d61afd03ff1662cff6412a28a86a7
SelinaZheng <71414455+SelinaZheng@users.noreply.github.com>
2022-06-02 12:14:40 -0400

    change the text

--- test/components/pages-comp/Invitee.vue
+++ test/components/pages-comp/Invitee.vue
@@ -41,15 +41,15 @@
             </div>
           </div>
           <div id="chat" class="chat">
-            <p class="friend chat">game created</p>
-            <p class="friend chat">confirm your presence with a message</p>
-            <p class="friend chat">
-              currently only compatible with 2 players only...
-            </p>
             <span class="space"> </span>
 
             <!-- here is the chat space i made -->
             <div class="chatList">
+              <p class="friend chat">game created</p>
+              <p class="friend chat">confirm your presence with a message</p>
+              <p class="friend chat">
+              currently only compatible with 2 players only...
+            </p>
               <div class="messages-container">
                 <div v-for="message in messages" :key="message.id">
                   <b>{{ message.user }}</b> :{{ message.text }}
